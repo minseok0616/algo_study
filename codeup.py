ecc8be0142f4b802190bb5730239003372f5f626
minseokChoi <minseok6815@naver.com>
2024-12-24 18:49:54 +0900

    6000-6010

---
+++ codeup.py
@@ -0,0 +1,26 @@
+# #6001
+# print("Hello")
+# #6002
+# print("Hello World")
+# #6003
+# print("Hello")
+# print("World")
+
+# print("Hello\nWorld")
+# #6004
+# print("'Hello'")
+# #6005
+# print('"Hello World"')
+# #6006
+# print("\"!@#$%^&*()\'")
+# #6007 큰따옴표 작은따옴표 구별하기
+# print("\"C:\\Download\\'hello\'.py\"")
+# print('"C:\Download\\\'hello\'.py\"')
+# #6008 공백문자 포함해서 출력하기
+# print("print(\"Hello\\nWorld\")")
+# #6009
+# a= input()
+# print(a)
+#6010
+n = int(input())
+print(n)
