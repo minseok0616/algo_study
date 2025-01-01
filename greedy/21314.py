815a6ea39fcbd9b5500bf57b53a2c5ee0b0726e5
minseokChoi <minseok6815@naver.com>
2025-01-01 14:43:25 +0900

    greedy

--- greedy/21314.py
+++ greedy/21314.py
@@ -1,3 +1,22 @@
-a = list(input())
+n = input()
+m = 0
+max_result = ''
+min_result = ''
+cnt = 0
+for i in n:
+    if i == 'K':
+        if m > 0:
+            max_result += str(5 * 10 ** m)
+            min_result += str(1 * 10 ** m + 5)
+            m = 0
+        else: 
+            max_result += '5'
+            min_result += '5'
 
-
+    else:
+        m += 1
+if m > 0: 
+    max_result += '1' * (m)
+    min_result += str(1*10**(m-1))
+print(max_result)
+print(min_result)
