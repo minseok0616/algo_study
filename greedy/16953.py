fe524f8d7f819c937a180367cb259311a583da38
minseokChoi <minseok6815@naver.com>
2025-01-01 12:44:30 +0900

    append

--- greedy/16953.py
+++ greedy/16953.py
@@ -1,23 +1,19 @@
-#정수 a를 정수 b로 변환하려고 하는데, 가능한 연산은 다음과 같이 두가지이다.
-# 2를 곱하는 연산
-# # 1을 수의 가장 오른족에 추가하는 연산
 A,B = map(int,input().split())
-while(B ==A):
-    if B % 2 == 0:
+count = 0
+check = 0
+while B != A and B>=A:
+    if B % 10 == 1: # 1이 나온경우를 뜻함.
+        B //= 10
+        count += 1
+    elif B % 2 == 0:
         B //= 2
-    else: # 홀수일때
-        B = str(B)
-        B.replace(B[-1],'')
+        count +=1
+    else:
+        check = 1
+        break
 
+if not check and A==B:
+    print(count + 1)
+else:
+    print(-1)
 
-
-
-
-
-# B = 123
-# B = str(B)
-# print(B.replace(B[-1],''))
-# print(B,type(B))
-# # print(B[0],B[1])
-# # B =int(B)
-# # print(B,type(B))
